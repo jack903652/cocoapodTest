1331fbb254d4ef8780baa7ba1722034d2993c828
chengang <ios_dev@iOS-devdeiMac.local>
2014-09-03 15:31:53 +0800

    Initial Commit

---
+++ cocoapodTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cocoapodTest
+//
+//  Created by chengang on 14-9-3.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cocoapodTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  cocoapodTest
+//
+//  Created by chengang on 14-9-3.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
